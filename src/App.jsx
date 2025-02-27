a44f74fc200dc65f361ce486c59ae32f5911e0b4
priy1105 <priyanshukr5912@gmail.com>
2025-02-27 15:14:42 +0530

    Updated the title in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { React } from 'react';
-import { BrowserRouter as Router, Routes, Route }  from 'react-router-dom' ;
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -7,7 +7,7 @@ import './App.scss';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/thoughtly2"> {/*repo name*/}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -18,5 +18,3 @@ function App() {
 }
 
 export default App;
-import './App.scss';
-
